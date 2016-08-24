8cd4c7307642a76452b7cc1a30bd1d5bd2d21a0c
Christian Le <christianle94@gmail.com>
2016-08-23 23:48:53 -0700

    Add functionality to handle array of options as found in .babelrcArray

--- test/index.js
+++ test/index.js
@@ -8,5 +8,19 @@ test((t) => {
         found: true,
         path: path.resolve(__dirname, 'somepath/file.js'),
     });
+
+    const result2 = resolve('@/file', __filename, {}, '.babelrcArray');
+    t.deepEqual(result2, {
+        found: true,
+        path: path.resolve(__dirname, 'somepath/file.js'),
+    });
+
+    const result3 = resolve('_/file', __filename, {}, '.babelrcArray');
+    t.deepEqual(result3, {
+        found: true,
+        path: path.resolve(__dirname, 'anotherpath/file.js'),
+    });
+
     t.end();
 });
+
