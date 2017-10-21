d67edf02e7f9a2de6d70d20f8357c0049f3d2c6c
cumul <gg6123@naver.com>
2017-10-21 15:46:56 +0900

    Added a test

--- test/index.js
+++ test/index.js
@@ -9,14 +9,20 @@ test((t) => {
         path: path.resolve(__dirname, 'somepath/file.js'),
     });
 
-    const result2 = resolve('@/file', __filename, {}, '.babelrcArray');
+    const result2 = resolve('@/file', __filename, { options: { rootPathSuffix: 'test/somepath', rootPathPrefix: '@' } });
     t.deepEqual(result2, {
         found: true,
         path: path.resolve(__dirname, 'somepath/file.js'),
     });
 
-    const result3 = resolve('_/file', __filename, {}, '.babelrcArray');
+    const result3 = resolve('@/file', __filename, { babelrc: '.babelrcArray' });
     t.deepEqual(result3, {
+        found: true,
+        path: path.resolve(__dirname, 'somepath/file.js'),
+    });
+
+    const result4 = resolve('_/file', __filename, { babelrc: '.babelrcArray' });
+    t.deepEqual(result4, {
         found: true,
         path: path.resolve(__dirname, 'anotherpath/file.js'),
     });
